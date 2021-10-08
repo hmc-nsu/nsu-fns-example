4f7768637e4020418515bfeebc775fd656580297
alex18209 <zmey25062000@gmail.com>
2021-10-08 18:29:18 +0600

    Добавлено пояснение к коду

--- jWork.py
+++ jWork.py
@@ -3,6 +3,7 @@ import random
 
 
 class Jwork:
+    #Ключевые слова для словаря
     shtamp = ['INN', 'PASSWORD', 'CLIENT_SECRET', 'INUSE']
 
     def __init__(self):
@@ -11,10 +12,12 @@ class Jwork:
         self.enduse=1
         self.lastuse=None
 
+    #Сохранить текущий словарь
     def saveDict(self):
         with open("fns_info.json", "w") as write_file:
             json.dump(self.data, write_file, indent=4)
 
+    #Создать словарь с иннн, поролем и секрктным полем
     def doDict(self, inn,password,client_secret)->dict:
         self.i+=1
         return {
@@ -26,6 +29,7 @@ class Jwork:
             }
         }
 
+    #Добавть одно использование к набору данных под номерои r
     def addUse(self,r):
         return {r: {
                 str(self.shtamp[0]): self.data.setdefault(r).setdefault(self.shtamp[0]),
@@ -35,6 +39,7 @@ class Jwork:
             }
         }
 
+    #Обнулить количество использований набора r
     def zeroedOne(self,r):
         return {r: {
                 str(self.shtamp[0]): self.data.setdefault(r).setdefault(self.shtamp[0]),
@@ -44,6 +49,7 @@ class Jwork:
             }
         }
 
+    #Вернуть словарь под номером r, причём только инн, пароль и секретное поле
     def partDickt(self,r):
         return {
             str(self.shtamp[0]): self.data.setdefault(r).setdefault(self.shtamp[0]),
@@ -51,27 +57,31 @@ class Jwork:
             self.shtamp[2]: self.data.setdefault(r).setdefault(self.shtamp[2]),
         }
 
-
+    #Добавить в json новые данные
     def saveNewData(self, inn,password,client_secret):
         self.loadDict();
         self.data.update(self.doDict(inn,password,client_secret))
         self.saveDict()
 
+    #Некоторые тестовые значения(потом изменить)
     def testData(self):
         self.saveNewData(111, 'qwerty', 'vegan')
         self.saveNewData(222, 'asdfgh', 'ne vegan')
 
+    #Загрузить словарь из json
     def loadDict(self):
         with open("fns_info.json", "r") as read_file:
             self.data = json.load(read_file)
         self.i=len(self.data)
 
+    #Получить словарь с данными для установления соединения. Если такого нет, то вернуть None
     def getInf(self)->dict:
         self.lastuse=self.getCanUse()
         if(self.lastuse==None):
             return None
         return self.partDickt(self.lastuse)
 
+    #Проверка, можно ли ещё использовать последний выбранный набор
     def tryToUse(self)->bool:
         r=self.lastuse
         if(int(self.data.setdefault(r).setdefault(self.shtamp[3])) >= self.enduse ):
@@ -80,6 +90,7 @@ class Jwork:
         self.saveDict()
         return True
 
+    #Получение номера набора, который можно использовать. Если такого нет, то вернуть None
     def getCanUse(self)->str:
         self.loadDict()
         myhave=list()
@@ -97,6 +108,7 @@ class Jwork:
             return r
         return None
 
+    #Обнулить количество использований
     def zeroedUse(self):
         self.loadDict()
 
@@ -104,6 +116,7 @@ class Jwork:
             self.data.update(self.zeroedOne(str(j+1)))
         self.saveDict()
 
+    #Обнулить количество использований всех данных
     def doEmpty(self):
         self.data=dict()
         self.saveDict()
